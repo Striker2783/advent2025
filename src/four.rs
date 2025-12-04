c249ddccbbd267fac99fa08c0a6d51b7ba971793
Striker2783 <owenxingli@gmail.com>
2025-12-04 09:43:23 -0500

    Day 4 Part 2

--- src/four.rs
+++ src/four.rs
@@ -2,49 +2,98 @@ use std::{fs, path::Path};
 
 pub fn run(p: &Path) {
     let s = fs::read_to_string(p).unwrap();
-    let v = parse(&s);
+    let mut v = parse(&s);
     println!("{}", solve1(&v));
+    println!("{}", solve2(&mut v));
+}
+
+fn can_remove(v: &[Vec<bool>], i: usize, j: usize) -> bool {
+    let b = v[i][j];
+    if !b {
+        return false;
+    }
+    let mut rolls = 0;
+    if i > 0 {
+        if j > 0 && v[i - 1][j - 1] {
+            rolls += 1;
+        }
+        if v[i - 1][j] {
+            rolls += 1;
+        }
+        if j + 1 < v[0].len() && v[i - 1][j + 1] {
+            rolls += 1;
+        }
+    }
+    if j > 0 && v[i][j - 1] {
+        rolls += 1;
+    }
+    if j + 1 < v[0].len() && v[i][j + 1] {
+        rolls += 1;
+    }
+    if i + 1 < v.len() {
+        if j > 0 && v[i + 1][j - 1] {
+            rolls += 1;
+        }
+        if v[i + 1][j] {
+            rolls += 1;
+        }
+        if j + 1 < v.len() && v[i + 1][j + 1] {
+            rolls += 1;
+        }
+    }
+    rolls < 4
 }
 
 fn solve1(v: &[Vec<bool>]) -> u32 {
     let mut count = 0;
     for (i, row) in v.iter().enumerate() {
-        for (j, b) in row.iter().cloned().enumerate() {
-            if !b {
-                continue;
-            }
-            let mut rolls = 0;
-            if i > 0 {
-                if j > 0 && v[i - 1][j - 1] {
-                    rolls += 1;
-                }
-                if v[i - 1][j] {
-                    rolls += 1;
-                }
-                if j + 1 < v[0].len() && v[i - 1][j + 1] {
-                    rolls += 1;
-                }
-            }
-            if j > 0 && v[i][j - 1] {
-                rolls += 1;
-            }
-            if j + 1 < v[0].len() && v[i][j + 1] {
-                rolls += 1;
-            }
-            if i + 1 < v.len() {
-                if j > 0 && v[i + 1][j - 1] {
-                    rolls += 1;
-                }
-                if v[i + 1][j] {
-                    rolls += 1;
-                }
-                if j + 1 < v.len() && v[i + 1][j + 1] {
-                    rolls += 1;
-                }
-            }
-            if rolls < 4 {
+        for (j, _) in row.iter().cloned().enumerate() {
+            if can_remove(v, i, j) {
                 count += 1;
             }
+        }
+    }
+    count
+}
+
+fn recursive_remove(v: &mut [Vec<bool>], i: usize, j: usize) -> u32 {
+    if !can_remove(v, i, j) {
+        return 0;
+    }
+    let mut count = 1;
+    v[i][j] = false;
+    if i > 0 {
+        if j > 0 {
+            count += recursive_remove(v, i - 1, j - 1);
+        }
+        count += recursive_remove(v, i - 1, j);
+        if j + 1 < v[0].len() {
+            count += recursive_remove(v, i - 1, j + 1);
+        }
+    }
+    if j > 0 {
+        count += recursive_remove(v, i, j - 1);
+    }
+    if j + 1 < v[0].len() {
+        count += recursive_remove(v, i, j + 1);
+    }
+    if i + 1 < v.len() {
+        if j > 0 {
+            count += recursive_remove(v, i + 1, j - 1);
+        }
+        count += recursive_remove(v, i + 1, j);
+        if j + 1 < v[0].len() {
+            count += recursive_remove(v, i + 1, j + 1);
+        }
+    }
+    count
+}
+
+fn solve2(v: &mut [Vec<bool>]) -> u32 {
+    let mut count = 0;
+    for i in 0..v.len() {
+        for j in 0..v[i].len() {
+            count += recursive_remove(v, i, j);
         }
     }
     count
@@ -58,7 +107,7 @@ fn parse(s: &str) -> Vec<Vec<bool>> {
 
 #[cfg(test)]
 mod tests {
-    use crate::four::{parse, solve1};
+    use crate::four::{parse, solve1, solve2};
 
     const TEST: &str = "..@@.@@@@.
 @@@.@.@.@@
@@ -73,7 +122,8 @@ mod tests {
 ";
     #[test]
     fn test_solve() {
-        let v = parse(TEST);
+        let mut v = parse(TEST);
         assert_eq!(solve1(&v), 13);
+        assert_eq!(solve2(&mut v), 43);
     }
 }
