e7fe9e8d0b96b56dbd1268aa228c7b3018572197
Striker2783 <owenxingli@gmail.com>
2025-12-05 09:22:13 -0500

    Day 5 Part 2

--- src/five.rs
+++ src/five.rs
@@ -4,6 +4,7 @@ pub fn run(p: &Path) {
     let s = fs::read_to_string(p).unwrap();
     let parsed = parse(&s);
     println!("{}", solve1(&parsed.0, &parsed.1));
+    println!("{}", solve2(&parsed.0));
 }
 
 fn parse(s: &str) -> (Vec<RangeInclusive<u64>>, Vec<u64>) {
@@ -35,9 +36,26 @@ fn solve1(r: &[RangeInclusive<u64>], v: &[u64]) -> u32 {
     count
 }
 
+fn solve2(r: &[RangeInclusive<u64>]) -> u64 {
+    let mut count = 0;
+    let mut r = r.to_vec();
+    r.sort_by(|a, b| a.start().cmp(b.start()));
+    let mut last = 0;
+    for r in r {
+        last = last.max(*r.start());
+        if *r.end() < last {
+            continue;
+        }
+        count += *r.end() - last + 1;
+        last = *r.end() + 1;
+    }
+
+    count
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::five::{parse, solve1};
+    use crate::five::{parse, solve1, solve2};
 
     const TEST: &str = "3-5
 10-14
@@ -54,5 +72,6 @@ mod tests {
     fn test_solve1() {
         let parsed = parse(TEST);
         assert_eq!(solve1(&parsed.0, &parsed.1), 3);
+        assert_eq!(solve2(&parsed.0), 14);
     }
 }
