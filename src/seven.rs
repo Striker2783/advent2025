86a32a8d1c8d7f7cc0e9465f12686cbfa7899e66
Striker2783 <owenxingli@gmail.com>
2025-12-07 11:52:12 -0500

    Day 7 Part 2

--- src/seven.rs
+++ src/seven.rs
@@ -4,6 +4,7 @@ pub fn run(p: &Path) {
     let s = fs::read_to_string(p).unwrap();
     let p = parse(&s);
     println!("{}", solve1(&p));
+    println!("{}", solve2(&p));
 }
 
 fn solve1(v: &[Vec<u8>]) -> u32 {
@@ -15,8 +16,8 @@ fn solve1(v: &[Vec<u8>]) -> u32 {
             if b != b'S' {
                 continue;
             }
-            match v[i+1][j] {
-                b'.' => v[i+1][j] = b'S',
+            match v[i + 1][j] {
+                b'.' => v[i + 1][j] = b'S',
                 b'^' => {
                     count += 1;
                     v[i + 1][j - 1] = b'S';
@@ -29,13 +30,48 @@ fn solve1(v: &[Vec<u8>]) -> u32 {
     count
 }
 
+fn solve2(v: &[Vec<u8>]) -> u64 {
+    let mut v = v.to_vec();
+    let mut v_count = vec![vec![0; v[0].len()]; v.len()];
+    for (i, v) in v.iter().enumerate() {
+        for (j, &b) in v.iter().enumerate() {
+            if b == b'S' {
+                v_count[i][j] += 1;
+            }
+        }
+    }
+    for i in 0..(v.len() - 1) {
+        for j in 0..v[i].len() {
+            let b = v[i][j];
+            if b != b'S' {
+                continue;
+            }
+            match v[i + 1][j] {
+                b'.' | b'S' => {
+                    v[i + 1][j] = b'S';
+                    v_count[i + 1][j] += v_count[i][j];
+                }
+                b'^' => {
+                    v[i + 1][j - 1] = b'S';
+                    v_count[i + 1][j - 1] += v_count[i][j];
+
+                    v[i + 1][j + 1] = b'S';
+                    v_count[i + 1][j + 1] += v_count[i][j];
+                }
+                _ => {}
+            }
+        }
+    }
+    v_count.last().unwrap().iter().sum()
+}
+
 fn parse(s: &str) -> Vec<Vec<u8>> {
     s.lines().map(|l| l.as_bytes().to_vec()).collect()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::seven::{parse, solve1};
+    use crate::seven::{parse, solve1, solve2};
 
     const TEST: &str = ".......S.......
 ...............
@@ -58,5 +94,6 @@ mod tests {
     fn test_solve() {
         let p = parse(TEST);
         assert_eq!(solve1(&p), 21);
+        assert_eq!(solve2(&p), 40);
     }
 }
