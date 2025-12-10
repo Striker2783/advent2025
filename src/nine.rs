c4312075d992e022e09c78bcd69dde3623b7d55a
Striker2783 <owenxingli@gmail.com>
2025-12-09 21:35:37 -0500

    Day 9 Part 2
    Found the simplest solution https://pastebin.com/S3wMpvCP .
    I thought of checking if any points were within the rectangle at first, but I did not think of checking the midpoints as well.

--- src/nine.rs
+++ src/nine.rs
@@ -18,26 +18,18 @@ fn solve1(v: &[(u64, u64)]) -> u64 {
     max
 }
 
-fn contains(v: &[(u64, u64)], corners: &[(u64, u64); 4]) -> bool {
-    'outer: for (&p1, &p2) in v.iter().zip(v.iter().skip(1).cycle()) {
-        for (&c1, &c2) in corners.iter().zip(corners.iter().skip(1).cycle()) {
-            if [p1, p2] == [c1, c2] {
-                continue 'outer;
-            } else if c1 == c2 {
-                continue;
-            }
-            // if (p1.0 == p2.0 && c1.0 == c2.0) || (p1.1 == p2.1 && c1.1 == c2.1) {
-            //     continue;
-            // }
-            if p1.0 == p2.0 {
-                if (p1.1.min(p2.1)..=p1.1.max(p2.1)).contains(&c1.1) {
-                    return false;
-                }
-            } else {
-                if (p1.0.min(p2.0)..=p1.0.max(p2.0)).contains(&c1.0) {
-                    return false;
-                }
-            }
+fn contains(v: &[(u64, u64)], c1: (u64, u64), c2: (u64, u64)) -> bool {
+    let min_p = (c1.0.min(c2.0), c1.1.min(c2.1));
+    let max_p = (c1.0.max(c2.0), c1.1.max(c2.1));
+    for (&p1, &p2) in v.iter().zip(v.iter().skip(1).cycle()) {
+        let within =
+            |p: (u64, u64)| p.0 > min_p.0 && p.0 < max_p.0 && p.1 > min_p.1 && p.1 < max_p.1;
+        if within(p1) || within(p2) {
+            return false;
+        }
+        let mid = ((p1.0 + p2.0) / 2, (p1.1 + p2.1) / 2);
+        if within(mid) {
+            return false;
         }
     }
     true
@@ -47,16 +39,9 @@ fn solve2(v: &[(u64, u64)]) -> u64 {
     let mut max = 0;
     for (i, &p1) in v.iter().enumerate() {
         for &p2 in v.iter().skip(i + 1) {
-            let corners = [
-                (p1.0.min(p2.0), p1.1.min(p2.1)),
-                (p1.0.min(p2.0), p1.1.max(p2.1)),
-                (p1.0.max(p2.0), p1.1.max(p2.1)),
-                (p1.0.max(p2.0), p1.1.min(p2.1)),
-            ];
-            if !contains(v, &corners) {
+            if !contains(v, p1, p2) {
                 continue;
             }
-            println!("{p1:?} {p2:?}");
             let area = (p1.0.abs_diff(p2.0) + 1) * (p1.1.abs_diff(p2.1) + 1);
             max = max.max(area);
         }
