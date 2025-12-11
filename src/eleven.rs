72d186ee0a361573a6faf3702c8f22341017fb3a
Striker2783 <owenxingli@gmail.com>
2025-12-11 08:44:52 -0500

    Day 11 Part 2

--- src/eleven.rs
+++ src/eleven.rs
@@ -1,13 +1,10 @@
-use std::{
-    collections::HashMap,
-    fs,
-    path::Path,
-};
+use std::{collections::HashMap, fs, path::Path};
 
 pub fn run(p: &Path) {
     let s = fs::read_to_string(p).unwrap();
     let p = parse(&s);
     println!("{}", solve1(&p));
+    println!("{}", solve2(&p));
 }
 
 fn parse(s: &str) -> HashMap<String, Vec<String>> {
@@ -30,12 +27,16 @@ fn recursive1(
     map: &HashMap<String, Vec<String>>,
     counts: &mut HashMap<String, u64>,
     curr: &str,
+    end: &str,
 ) -> u64 {
-    if curr == "out" {
+    if curr == end {
         return 1;
     }
+    if let Some(&c) = counts.get(curr) {
+        return c;
+    }
     if let Some(v) = map.get(curr) {
-        let count: u64 = v.iter().map(|s| recursive1(map, counts, s)).sum();
+        let count: u64 = v.iter().map(|s| recursive1(map, counts, s, end)).sum();
         counts.insert(curr.to_string(), count);
         return count;
     }
@@ -44,12 +45,22 @@ fn recursive1(
 
 fn solve1(map: &HashMap<String, Vec<String>>) -> u64 {
     let mut counts = HashMap::new();
-    recursive1(map, &mut counts, "you")
+    recursive1(map, &mut counts, "you", "out")
+}
+
+fn solve2(map: &HashMap<String, Vec<String>>) -> u64 {
+    let a = recursive1(map, &mut HashMap::new(), "svr", "dac");
+    let b = recursive1(map, &mut HashMap::new(), "dac", "fft");
+    let c = recursive1(map, &mut HashMap::new(), "fft", "out");
+    let a2 = recursive1(map, &mut HashMap::new(), "svr", "fft");
+    let b2 = recursive1(map, &mut HashMap::new(), "fft", "dac");
+    let c2 = recursive1(map, &mut HashMap::new(), "dac", "out");
+    a * b * c + a2 * b2 * c2
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::eleven::{parse, solve1};
+    use crate::eleven::{parse, solve1, solve2};
 
     const TEST: &str = "aaa: you hhh
 you: bbb ccc
@@ -62,9 +73,26 @@ ggg: out
 hhh: ccc fff iii
 iii: out
 ";
+
+    const TEST2: &str = "svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out
+";
     #[test]
     fn test_solve() {
         let map = parse(TEST);
         assert_eq!(solve1(&map), 5);
+        let map = parse(TEST2);
+        assert_eq!(solve2(&map), 2);
     }
 }
